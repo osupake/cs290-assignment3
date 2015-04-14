4c8e19dbce1e9546f4a9b25d832f669d107dd2b4
osupake <epak811@gmail.com>
2015-04-14 18:18:59 -0400

    finished variables.js

--- src/variables.js
+++ src/variables.js
@@ -15,6 +15,10 @@ function variableModification(a) {
   var yourNumberIs;
   //your code here
 
+  plus5 = a + 5;
+  asString = "" + a;
+  yourNumberIs = "Your Number is " + a;
+
   //end your code
   return [plus5, asString, yourNumberIs, a];
 }
@@ -29,7 +33,12 @@ false otherwise
 */
 function isString(b) {
   //your code here
-  return undefined;
+  if(typeof b === "string") {
+    return true;
+  }
+  else {
+    return false;
+  }
   //end your code
 }
 
@@ -42,6 +51,11 @@ return true if c is null, false otherwise
 */
 function isNull(c) {
   //your code here
-  return undefined;
+  if(c === null) {
+    return true;
+  }
+  else {
+    return false;
+  }
   //end your code
 }
